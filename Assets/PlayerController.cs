6d998d3198281b02bc4deb305b8e5bb33166c114
LiliNguyen1612 <nnanhduong1612@gmail.com>
2024-05-17 14:20:53 +0700

    14. Collision Detection

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -10,20 +12,34 @@ public class PlayerController : MonoBehaviour
 
     private float movingInput;
 
-    void Start()
-    {
-
-    }
+    public LayerMask whatIsGround;
+    public float groundCheckDistance;
+    private bool isGrounded;
 
     void Update()
     {
+        CollisionCheck();
+        
         movingInput = Input.GetAxis("Horizontal");
-
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            if (isGrounded)
+            {
+                Jump();
+            }
         }
-
-        rb.velocity = new Vector2(moveSpeed * movingInput, rb.velocity.y);
+        Move();
     }
+    private void Move()         => rb.velocity = new Vector2(moveSpeed * movingInput, rb.velocity.y);
+
+    private void Jump()         => rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+
+    private bool CollisionCheck()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+        return isGrounded;
+    }
+
+
+    private void OnDrawGizmos() => Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundCheckDistance));
 }
